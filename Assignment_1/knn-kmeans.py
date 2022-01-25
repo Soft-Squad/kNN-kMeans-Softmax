d6c80e027d24dba966d05f4e22420cdd2649544d
Alex <wintersalex@icloud.com>
2022-01-24 18:21:41 -0800

    Adding the CIFAR-10 Dataset files. Data loads properly Distance can be calculated

--- Assignment_1/knn-kmeans.py
+++ Assignment_1/knn-kmeans.py
@@ -1,24 +1,38 @@
-import sys
-import pickle
-import numpy
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms  # Contains the CIFAR-10 Dataset
+from torchvision.transforms import ToTensor, Compose  # Tensor is used to encode the inputs and outputs
+import numpy as np
+import matplotlib.pyplot as plt     # Used to help visualize data
 
 
-def unpickle_cifar10(file):
-    # Each of the batch file contains a dictionary with the following elements:
-    # data -- 10000x3072 numpy array of uint8S.
-    # Each row stores a 32x32 colour image. The first 1024 entries contain the red channel values,
-    # the next 1024 the green, and the final 1024 the blue. The image is stored in row-major order, so that the first
-    # 32 entries of the array are the red channel values of the first row of the image.
-    # labels -- a list of 10000 numbers in the range 0-9.
-    # The number at index i indicates the label of the ith image in the array data.
-    with open(file, 'rb') as fo:
-        cifar_dict = pickle.load(fo, encoding='bytes')
-    return cifar_dict
+def knn(X):
+    k = 3
+    distances = np.linalg.norm(X[0] - X[1])
+    print(distances)
+    return None
 
 
 def main():
-    unpickle_cifar10(sys.argv[1])
-    print("Test")
+    # Download CIFAR-10 to a folder called 'data'
+    train_dataset = datasets.CIFAR10(root='data/', download=True, train=True, transform=ToTensor())
+    test_dataset = datasets.CIFAR10(root='data/', download=True, train=False, transform=ToTensor())
+
+    np.set_printoptions(threshold=np.inf)   # Allows printing of 50000 items
+
+    # Get data and labels from the dataset
+    x_train = train_dataset.data
+    y_train = np.array(train_dataset.targets)
+    x_train_reshaped = x_train.reshape((50000, 32 * 32 * 3)).astype('float')  # In the form of a np.array
+
+    x_test = test_dataset.data
+    y_test = np.array(test_dataset.targets)
+    x_test_reshaped = x_test.reshape((10000, 32 * 32 * 3)).astype('float')  # In the form of a np.array
+
+    classes = train_dataset.classes     # Image class names from CIFAR-10
+
+    knn(x_train_reshaped)
 
 
 if __name__ == '__main__':
