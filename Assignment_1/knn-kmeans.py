17182007fabf83ed6333c129bd4c23287d31ddb7
Alexander <wintersalex@icloud.com>
2022-01-27 15:12:43 -0800

    kNN implementation is complete Can merge with main

--- Assignment_1/knn-kmeans.py
+++ Assignment_1/knn-kmeans.py
@@ -5,13 +5,28 @@ from torchvision import datasets, transforms  # Contains the CIFAR-10 Dataset
 from torchvision.transforms import ToTensor, Compose  # Tensor is used to encode the inputs and outputs
 import numpy as np
 import matplotlib.pyplot as plt     # Used to help visualize data
+import scipy.stats
 
 
-def knn(X):
-    k = 3
-    distances = np.linalg.norm(X[0] - X[1])
-    print(distances)
-    return None
+def compute_distances(X_Train, X):
+    # Distances formula from: https://medium.com/@souravdey/l2-distance-matrix-vectorization-trick-26aa3247ac6c
+    distances = -2 * np.dot(X, X_Train.T) + np.sum(X_Train**2, axis=1) + np.sum(X**2, axis=1)[:, np.newaxis]
+    return distances
+
+
+def y_predict(y_train, dists, k=1):
+    #print(k)
+    num_test = dists.shape[0]
+    #print(num_test)
+    y_pred = np.zeros(num_test)
+    for i in range(num_test):
+        closest_y = []
+        nearest_neighbor_ids = np.argsort(dists[i])
+        closest_y = list(y_train[nearest_neighbor_ids[:k]])
+        pass
+        y_pred[i] = (np.argmax(np.bincount(closest_y)))
+        pass
+    return y_pred
 
 
 def main():
@@ -30,9 +45,16 @@ def main():
     y_test = np.array(test_dataset.targets)
     x_test_reshaped = x_test.reshape((10000, 32 * 32 * 3)).astype('float')  # In the form of a np.array
 
+    num_test = x_test_reshaped.shape[0]
+
     classes = train_dataset.classes     # Image class names from CIFAR-10
 
-    knn(x_train_reshaped)
+    distances = compute_distances(x_train_reshaped, x_test_reshaped)
+    y_prediction = y_predict(y_train, distances, k=11)
+
+    correct = np.sum(y_prediction == y_test)
+    accuracy = (float(correct) / num_test) * 100
+    print("Correct: %d/%d\nAccuracy: %f" % (correct, num_test, accuracy))
 
 
 if __name__ == '__main__':
